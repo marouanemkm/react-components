ef2b7a58c2487cf8410cd783fa77c9ea15703bac
Marouane <maroumkm@gmail.com>
2021-09-29 19:00:44 +0200

    Ajout de la Navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -3,17 +3,36 @@ import './Navbar.css';
 
 export default function Navbar() {
 
-    const [toggleMenu, setToggleMenu] = useState();
+    const [toggleMenu, setToggleMenu] = useState(false);
+    const [largeur, setLargeur] = useState(window.innerWidth);
+
+    const toggleNav = () => {
+        setToggleMenu(!toggleMenu);
+    };
+
+    useEffect(()=> {
+        const changeWidth = () => {
+            setLargeur(window.innerWidth);
+        }
+
+        window.addEventListener('resize', changeWidth);
+
+        return () => {
+            window.removeEventListener('resize', changeWidth);
+        }
+    }, []);
 
 
     return (
         <nav>
+        {(toggleMenu || largeur > 500) && (
             <ul className="liste">
                 <li className="items">Accueil</li>
                 <li className="items">Service</li>
                 <li className="items">Contact</li>
             </ul>
-            <button className="btn">Bouton</button>
+        )}
+            <button onClick={toggleNav} className="btn">Bouton</button>
         </nav>
     );
 };
